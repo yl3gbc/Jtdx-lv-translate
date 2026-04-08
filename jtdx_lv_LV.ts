5f03c2a6c3868f9571ef2b2b758ad9221db7b4e6
Gabriels <gabriels.breidaks@gmail.com>
2026-04-08 22:52:21 +0300

    Pievienot darbu3
    Signed-off-by: Gabriels <gabriels.breidaks@gmail.com>

--- jtdx_lv_LV.ts
+++ jtdx_lv_LV.ts
@@ -6357,7 +6357,7 @@ Error(%3): %4</source>
     <message>
         <location filename="../SampleDownloader/RemoteFile.cpp" line="193"/>
         <source>Too many redirects: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Pārāk daudz novirzījumu: %1</translation>
     </message>
     <message>
         <location filename="../SampleDownloader/RemoteFile.cpp" line="205"/>
